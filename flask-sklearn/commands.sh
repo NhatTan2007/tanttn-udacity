55b5d9b17830e86f54d29fad5d069f523700c8cd
TanTTN <nhattan2007@gmail.com>
2023-11-09 00:00:19 +0700

    Update script

--- flask-sklearn/commands.sh
+++ flask-sklearn/commands.sh
@@ -8,7 +8,7 @@ azWebAppRuntime="PYTHON:3.8"
 
 az group create -n $resourceGroup -l $location
 
-az appservice plan create -n $azWebAppPlan -g $resourceGroup --is-linux --sku FREE -l $location
+az appservice plan create -n $azWebAppPlan -g $resourceGroup --is-linux --sku B1 -l $location
 
 az webapp create -n $azWebApp -p $azWebAppPlan -g $resourceGroup --runtime $azWebAppRuntime
 
