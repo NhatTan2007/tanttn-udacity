a3dbe5fba717e67cfa95347bce0d977472f5301a
TanTTN <nhattan2007@gmail.com>
2023-11-08 23:21:35 +0700

    Update script

--- flask-sklearn/commands.sh
+++ flask-sklearn/commands.sh
@@ -1,11 +1,15 @@
-resourceGroup='tanttn-udacity'
-location='eastus'
-azWebApp='tanttnudacity'
-azWebAppPlan='tanttnudacity'
-azWebAppRuntime='PYTHON:3.8'
+#!/bin/bash
+
+resourceGroup="tanttn-udacity"
+location="eastus"
+azWebApp="tanttnudacity"
+azWebAppPlan="tanttnudacity"
+azWebAppRuntime="PYTHON:3.8"
 
 az group create -n $resourceGroup -l $location
 
 az appservice plan create -n $azWebAppPlan -g $resourceGroup --is-linux --sku FREE -l $location
 
 az webapp create -n $azWebApp -p $azWebAppPlan -g $resourceGroup --runtime $azWebAppRuntime
+
+az webapp up -n $azWebApp --runtime $azWebAppRuntime
