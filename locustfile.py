55a7a52d22fd8b17b0c5a252f49642464f611589
TanTTN <nhattan2007@gmail.com>
2023-11-11 15:47:18 +0700

    Update locust file

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task
 
 class UserBehavior(TaskSet):
     def on_start(self):
@@ -8,7 +8,7 @@ class UserBehavior(TaskSet):
     def index(self):
          self.client.get("/")
 
-class WebsiteUser(HttpLocust):
+class WebsiteUser(HttpUser):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
