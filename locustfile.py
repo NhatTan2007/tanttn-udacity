1173b585a6ba243f617960a102b720a500f578de
TanTTN <nhattan2007@gmail.com>
2023-11-12 15:17:32 +0700

    install locust

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,37 @@
-from locust import HttpUser, TaskSet, task
+import time
+from locust import HttpUser, task, between
 
-class UserBehavior(TaskSet):
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task(3)
+    def predict(self):
+        for item_id in range(10):
+            self.client.post("/predict", json={  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+})
+            time.sleep(1)
+
     def on_start(self):
         self.index()
-
-    @task(1)
-    def index(self):
-         self.client.get("/")
-
-class WebsiteUser(HttpUser):
-    task_set = UserBehavior
-    min_wait = 5000
-    max_wait = 9000
-
